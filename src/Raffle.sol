65fa993c124ca4c63e0a804f5404832a7852872d
Yash Garg <yashygkf@gmail.com>
2023-08-25 20:15:34 +0530

    feat: intialised chianlink

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -22,8 +22,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 /**
  * @title Raffle
@@ -35,32 +34,50 @@ contract Raffle {
     // Errors
     error Raffle__IncorrectEnteranceFee();
 
-
     // State variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_keyHash;
+    uint32 private immutable i_callbackGasLimit;
+    uint64 private immutable i_subscriptionId;
     uint256 private immutable i_enteranceFee;
     uint256 private immutable i_durationInSeconds;
+
     uint256 private s_lastTimeStamp;
     address payable[] private s_participants;
 
+    uint32 private constant NUM_WORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
 
     // Events
     event Raffle__ParticipantEntered(address indexed participant);
 
-    constructor(uint256 _enteranceFee, uint256 _durationInSeconds) {
+    constructor(
+        uint256 _enteranceFee,
+        uint256 _durationInSeconds,
+        address _vrfCoordinator,
+        bytes32 _keyHash,
+        uint32 _callbackGasLimit,
+        uint64 _subscriptionId
+    ) {
         i_enteranceFee = _enteranceFee;
         i_durationInSeconds = _durationInSeconds;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinator);
+        i_keyHash = _keyHash;
+        i_callbackGasLimit = _callbackGasLimit;
+        i_subscriptionId = _subscriptionId;
+
         s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
-        if(msg.value < i_enteranceFee) revert Raffle__IncorrectEnteranceFee();
-        
+        if (msg.value < i_enteranceFee) revert Raffle__IncorrectEnteranceFee();
+
         s_participants.push(payable(msg.sender));
         emit Raffle__ParticipantEntered(msg.sender);
     }
 
     function pickWinner() external {
-        if ((block.timestamp - s_lastTimeStamp) <  i_durationInSeconds) {
+        if ((block.timestamp - s_lastTimeStamp) < i_durationInSeconds) {
             revert();
         }
     }
