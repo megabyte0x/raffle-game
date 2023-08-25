af7a42e6b9f97c6babb6b36d72dd8ac2b8006d91
Yash Garg <yashygkf@gmail.com>
2023-08-25 20:44:20 +0530

    feat: chainlink VRF implemented

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -23,6 +23,7 @@
 pragma solidity ^0.8.18;
 
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 /**
  * @title Raffle
@@ -30,9 +31,18 @@ import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interface
  * @notice This contract is used to create a raffle
  * @dev This contract implements Chainlink VRF2
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     // Errors
     error Raffle__IncorrectEnteranceFee();
+    error Raffle__TransferFailed();
+    error Raffle__NotOpen();
+
+    /** Type Declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING_WINNER,
+        CLOSED
+    }
 
     // State variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
@@ -44,12 +54,15 @@ contract Raffle {
 
     uint256 private s_lastTimeStamp;
     address payable[] private s_participants;
+    address payable private s_recentWinner;
+    RaffleState private s_raffleState;
 
     uint32 private constant NUM_WORDS = 1;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
 
     // Events
     event Raffle__ParticipantEntered(address indexed participant);
+    event Raffle__WinnerPicked(address indexed winner);
 
     constructor(
         uint256 _enteranceFee,
@@ -58,7 +71,7 @@ contract Raffle {
         bytes32 _keyHash,
         uint32 _callbackGasLimit,
         uint64 _subscriptionId
-    ) {
+    ) VRFConsumerBaseV2(_vrfCoordinator) {
         i_enteranceFee = _enteranceFee;
         i_durationInSeconds = _durationInSeconds;
         i_vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinator);
@@ -71,7 +84,7 @@ contract Raffle {
 
     function enterRaffle() external payable {
         if (msg.value < i_enteranceFee) revert Raffle__IncorrectEnteranceFee();
-
+        if (s_raffleState != RaffleState.OPEN) revert Raffle__NotOpen();
         s_participants.push(payable(msg.sender));
         emit Raffle__ParticipantEntered(msg.sender);
     }
@@ -80,9 +93,35 @@ contract Raffle {
         if ((block.timestamp - s_lastTimeStamp) < i_durationInSeconds) {
             revert();
         }
+        s_raffleState = RaffleState.CALCULATING_WINNER;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_keyHash,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
     }
 
     function getEnteranceFee() external view returns (uint256) {
         return i_enteranceFee;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        uint256 indexOfWinner = _randomWords[0] % s_participants.length;
+        address payable winner = s_participants[indexOfWinner];
+        s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+
+        s_participants = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) revert Raffle__TransferFailed();
+
+        emit Raffle__WinnerPicked(winner);
     }
 }
